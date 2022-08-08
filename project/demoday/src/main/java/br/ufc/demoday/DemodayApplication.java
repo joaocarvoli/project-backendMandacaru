5d97471514ba2c1321adfcb02936771fca800448
danielogeronimo <danielogeronimo@gmail.com>
2022-08-08 20:55:36 -0300

    Envio de projeto

---
+++ project/demoday/src/main/java/br/ufc/demoday/DemodayApplication.java
@@ -0,0 +1,13 @@
+package br.ufc.demoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodayApplication.class, args);
+	}
+
+}
