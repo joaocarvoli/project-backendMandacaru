737aa729decdfeae446afea5cd8d6ee85d8c90f6
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-09 10:47:08 -0300

    Estruturando os services

---
+++ project/demoday/src/main/java/br/ufc/demoday/service/AdService.java
@@ -0,0 +1,8 @@
+package br.ufc.demoday.service;
+import br.ufc.demoday.repository.AdRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AdService {
+}
---
+++ project/demoday/src/main/java/br/ufc/demoday/service/ImmobileService.java
@@ -0,0 +1,8 @@
+package br.ufc.demoday.service;
+import br.ufc.demoday.repository.ImmobileRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ImmobileService {
+}
