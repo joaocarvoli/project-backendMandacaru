2bbbb458f9c15ca8f30fab7a94b987368c292410
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-09 20:42:15 -0300

    Classe de Imóvel que terá relação com o anúncio

--- project/demoday/src/main/java/br/ufc/demoday/model/Immobile.java
+++ project/demoday/src/main/java/br/ufc/demoday/model/Immobile.java
@@ -1,4 +1,101 @@
 package br.ufc.demoday.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Immobile {
+    @Id
+    @GeneratedValue
+    private int idImmobile;
+    private String title;
+    private String address;
+    private double squareMetersGround;
+    private double squareMetersBuilt;
+    private int roomsAmount;
+    private int garageAmount;
+
+    public Immobile() {
+        super();
+    }
+
+    public Immobile(int idImmobile, String title, String address, double squareMetersGround, double squareMetersBuilt, int roomsAmount, int garageAmount) {
+        super();
+        this.idImmobile = idImmobile;
+        this.title = title;
+        this.address = address;
+        this.squareMetersGround = squareMetersGround;
+        this.squareMetersBuilt = squareMetersBuilt;
+        this.roomsAmount = roomsAmount;
+        this.garageAmount = garageAmount;
+    }
+
+    public int getIdImmobile() {
+        return idImmobile;
+    }
+
+    public void setIdImmobile(int idImmobile) {
+        this.idImmobile = idImmobile;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSquareMetersGround() {
+        return squareMetersGround;
+    }
+
+    public void setSquareMetersGround(double squareMetersGround) {
+        this.squareMetersGround = squareMetersGround;
+    }
+
+    public double getSquareMetersBuilt() {
+        return squareMetersBuilt;
+    }
+
+    public void setSquareMetersBuilt(double squareMetersBuilt) {
+        this.squareMetersBuilt = squareMetersBuilt;
+    }
+
+    public int getRoomsAmount() {
+        return roomsAmount;
+    }
+
+    public void setRoomsAmount(int roomsAmount) {
+        this.roomsAmount = roomsAmount;
+    }
+
+    public int getGarageAmount() {
+        return garageAmount;
+    }
+
+    public void setGarageAmount(int garageAmount) {
+        this.garageAmount = garageAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Immobile{" +
+                "idImmobile=" + idImmobile +
+                ", title='" + title + '\'' +
+                ", address='" + address + '\'' +
+                ", squareMetersGround=" + squareMetersGround +
+                ", squareMetersBuilt=" + squareMetersBuilt +
+                ", roomsAmount=" + roomsAmount +
+                ", garageAmount=" + garageAmount +
+                '}';
+    }
 }
