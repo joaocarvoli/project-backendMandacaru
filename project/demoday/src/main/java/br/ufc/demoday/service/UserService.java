21f054b0899dfb58fd3c76d84d2fb861644f0250
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-11 01:43:04 -0300

    Criando a classe service que interage com os repositories para o User

--- project/demoday/src/main/java/br/ufc/demoday/service/UserService.java
+++ project/demoday/src/main/java/br/ufc/demoday/service/UserService.java
@@ -1,8 +1,43 @@
 package br.ufc.demoday.service;
 import br.ufc.demoday.repository.UserRepository;
+import br.ufc.demoday.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public void save(int idUser, User entity){
+        if (idUser != 0) {
+            entity.setIdUser(idUser);
+        }
+        userRepository.save(entity);
+    }
+
+    public User find(int idUser){
+        if(idUser == 0){
+            return null;
+        }
+        Optional<User> user = userRepository.findById(idUser);
+        if(user.isPresent()){
+            return user.get();
+        }
+        return null;
+    }
+
+    public ArrayList<User> findAll(){
+        return (ArrayList<User>) userRepository.findAll();
+    }
+
+    public void delete(int idUser){
+        Optional<User> user = userRepository.findById(idUser);
+        if(user.isPresent()){
+            userRepository.delete(user);
+        }
+    }
 }
