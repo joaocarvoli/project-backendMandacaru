48af82dfe52c35da1ff8e6fc694d593f2f535c1a
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-16 20:33:52 -0300

    formatação de código

--- project/demoday/src/main/java/br/ufc/demoday/service/ImmobileService.java
+++ project/demoday/src/main/java/br/ufc/demoday/service/ImmobileService.java
@@ -45,12 +45,10 @@ public class ImmobileService {
     }
 
     public void delete(int idImmobile){
-        Optional<Immobile> immobile = immobileRepository.findById(idImmobile); // Necessário utilizar o método find e instanciar o objeto immobile sem o container Optional 
-
+        Optional<Immobile> immobile = immobileRepository.findById(idImmobile); // Necessário utilizar o método find e instanciar o objeto immobile sem o container Optional
         if(immobile.isPresent()){
             immobileRepository.delete(immobile.get());
         }
-
     }
 
 }
