9b39e0121e08fdede2bda24773afbcb8a8fe889e
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-30 21:37:40 -0300

    Forçando a autênticação nos controllers

--- project/demoday/src/main/java/br/ufc/demoday/controller/KeycloakControllerLogin.java
+++ project/demoday/src/main/java/br/ufc/demoday/controller/KeycloakControllerLogin.java
@@ -1,5 +1,6 @@
 package br.ufc.demoday.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //@RequestMapping(path = "/")
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class KeycloakControllerLogin {
 
 
